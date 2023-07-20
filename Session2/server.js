b32a6b4bf3f6de64c6c96246b58a0e8ce8253925
Chu Thanh Hung <chuthanh.hung@petronas.com>
2023-07-21 03:11:53 +0700

    new folders

---
+++ Session2/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+app.listen(3000);
+
+app.get("/", function(req, res) {
+    res.send("Hello");
+});
+
+app.get("/register", function(req, res) {
+    // res.send({ page: "register", status: "success" });
+    res.json({ page: "register", status: "success" });
+})
+
+app.post("/register", function(req, res) {
+    res.send("PostMethod");
+})
+
+app.get("/register/:un/:pw", function(req, res) {
+    var username = req.params.un;
+    var password = req.params.pw;
+    console.log(username);
+    res.json({ username: username, password: password });
+})
