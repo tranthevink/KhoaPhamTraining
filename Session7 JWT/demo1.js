f878502704b4b944e8f16b5a466c8bade0737c85
Chu Thanh Hung <chuthanh.hung@petronas.com>
2023-08-11 18:35:30 +0700

    add

--- Session7 JWT/demo1.js
+++ Session7 JWT/demo1.js
@@ -3,7 +3,7 @@ const privateKey = "fsfsdfsfsdfewfwefwef#@$#%^%$^%$YG%$Y%G%GREGERGERG";
 var secretData = {
     name: "The Vi Tran",
     courses: [{ name: "Android" }, { name: "Blockchain" }],
-    registerDate: Date.now()
+    // registerDate: Date.now()
 };
 
 jwt.sign(secretData, privateKey, function(err, token) {
---
+++ Session7 JWT/demo2.js
@@ -0,0 +1,3 @@
+var jwt = require('jsonwebtoken');
+const privateKey = "fsfsdfsfsdfewfwefwef#@$#%^%$^%$YG%$Y%G%GREGERGERG";
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVGhlIFZpIFRyYW4iLCJjb3Vyc2VzIjpbeyJuYW1lIjoiQW5kcm9pZCJ9LHsibmFtZSI6IkJsb2NrY2hhaW4ifV0sImlhdCI6MTY5MTc1MzcwMH0.vZPNh7xWzgjHD9dcKH7RIHbd1HKqgD0q7NvPkRsX2BU"
