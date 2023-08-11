7be0eab52ef395b271672e424a06b709e599c25d
Chu Thanh Hung <chuthanh.hung@petronas.com>
2023-08-11 19:20:50 +0700

    add

---
+++ Session7 JWT/demo3.js
@@ -0,0 +1,3 @@
+//http: //localhost:3000/sum/5/4
+// { total: 10, no1: 6, no2: 4 } > token > save cookies token: ....
+//http: //localhost:3000/verifyToken (post, body containing token, return successfully or failed)
