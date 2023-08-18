290d69c50feadc3aee4ed53dafd5b970ddfdda77
Chu Thanh Hung <chuthanh.hung@petronas.com>
2023-08-18 17:40:03 +0700

    add

--- Session8 Wines/routes/homePage/main.js
+++ Session8 Wines/routes/homePage/main.js
@@ -2,42 +2,59 @@ var User = require("../../models/User");
 var bcrypt = require('bcryptjs');
 
 
-module.exports = function(appParam) {
+module.exports = function(appParam, isEmailValid, config) {
     appParam.get("/", (req, res) => {
-        var myUser = new User({
-            Email: String,
-            Password: String,
-
-            Avatar: String,
-
-            Active: Boolean,
-            RegisterDate: Date,
-
-            Socket: String
-        });
-        res.json({ sayHi: "Hello" });
+        res.send("Hello world");
     });
-    appParam.post("/register", function(req, res) {
-        console.log(req);
-        if (!req.body.Email || !req.body.Password) {
-            res.json({ result: 0, message: "Wrong parameters" })
-        } else {
-            bcrypt.genSalt(10, function(err, salt) { //the number of 10 or 15 shows the difficulty of 
-                bcrypt.hash(req.body.Password, salt, function(err, hash) {
-                    var myUser = new User({
-                        Email: req.body.Email,
-                        Password: hash
-                    });
-                    myUser.save()
-                        .then((data) => {
-                            res.json({ result: 1, message: "Save new user successfully.", data: data });
-                        })
-                        .catch((err) => {
-                            res.json({ result: 0, message: "Save new user failed.", data: err });
-                        });
-                });
-            });
 
+    appParam.post("/register", function(req, res) {
+        //user uses for registering with 
+        //Email: String, Password: String
+        // Return: {result:0/1, mesage:""}
+        if (!req.body.Email || !req.body.Password) {
+            res.json({ result: 0, message: "Wrong parameters!" })
+        } else {
+            var email = req.body.Email.toLowerCase();
+            var password = req.body.Password;
+            //check email template
+            if (!isEmailValid(email)) {
+                res.json({ result: 0, message: "Wrong email!" });
+            } else if (password.length < config.validateFormat.minPasswordLength) {
+                //Note: Check do phuc tap cuar pw: PHai co Number, Text, Uppercase, 
+                res.json({ result: 0, message: "Wrong password's length!" })
+            } else {
+                //check existing email
+                User.findOne({}).then((data) => {
+                    if (data != null) {
+                        res.json({ result: 0, message: "The email was used!" })
+                    } else {
+                        bcrypt.genSalt(config.saltRounds, function(err, salt) {
+                            bcrypt.hash(password, salt, function(err, hash) {
+                                if (err) {
+                                    res.json({ result: 0, message: "Password hash failed!" })
+                                } else {
+                                    //save new user to database
+                                    var newUser = new User({
+                                        Email: email,
+                                        Password: hash,
+                                        Avatar: "avatar.jpg",
+                                        RegisterDate: Date.now(),
+                                        Socket: ""
+                                    });
+                                    newUser.save().then((data) => {
+                                        res.json({ result: 1, message: "Registered successfully!", user: data });
+                                    }).catch((err) => {
+                                        res.json({ result: 0, message: "User saved error!" });
+                                    })
+                                }
+                            });
+                        });
+
+                    }
+                }).catch((err) => {
+                    res.json({ result: 0, message: "Check Email existing failed!" })
+                })
+            }
         }
-    })
+    });
 };
--- Session8 Wines/server.js
+++ Session8 Wines/server.js
@@ -22,9 +22,37 @@ fs.readFile("./config.json", (err, data) => {
         mongoose.connect(config.dbConnectionString)
             .then(() => {
                 console.log("Mongodb connected successfully!!!!");
-                require("./routes/homePage/main")(app);
+                require("./routes/homePage/main")(app, isEmailValid, config);
             }).catch((err) => {
                 console.log("Mongodb connected failed!!!! " + err);
             });
     }
 });
+
+
+function isEmailValid(email) {
+    if (!email)
+        return false;
+    // console.log("1");
+    if (email.length > 254)
+        return false;
+    // console.log("2");
+
+    var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+    var valid = emailRegex.test(email);
+    if (!valid)
+        return false;
+    // console.log("3");
+
+    // Further checking of some things regex can't handle
+    var parts = email.split("@");
+    if (parts[0].length > 64)
+        return false;
+    // console.log("4");
+
+    var domainParts = parts[1].split(".");
+    if (domainParts.some(function(part) { return part.length > 63; }))
+        return false;
+    // console.log("5");
+    return true;
+}
